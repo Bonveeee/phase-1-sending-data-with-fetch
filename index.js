892c7f4b3798b5efd6614d8c44a98458d03e597b
bonveeee <bonochieng@gmail.com>
2022-06-17 01:09:01 +0300

    commit tree 6d7f251be7d644d7ae4202a5424b8878e409dd8c

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+let formData = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+
+function submitData(name , email) {
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com",
+        }),
+    };
+
+   return  fetch("http://localhost:3000/users", configurationObject)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            document.body.innerHTML += `<p>${data.id}</p>`;
+        })
+        .catch((error) => {
+            alert("Ooops!error");
+            console.log(error.message);
+            document.body.innerHTML += `<p>${error.message}</p>`;
+        });
+}
